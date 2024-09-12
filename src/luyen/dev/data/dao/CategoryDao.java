4e3bb00dfcad91c26da7db1bc506ed33d1e4049c
Luyen152 <nvl0932498215@gmail.com>
2024-09-12 16:34:14 +0700

    MySQLDao

---
+++ src/luyen/dev/data/dao/CategoryDao.java
@@ -0,0 +1,12 @@
+package luyen.dev.data.dao;
+
+import java.util.List;
+import luyen.dev.data.model.Category;
+public interface CategoryDao {
+    public boolean insert(Category category);
+    public boolean update(Category category);
+    public boolean delete(int id);
+    public Category find(int id);
+    public List<Category> findAll();
+    public List<Category> hotCategory();
+}
